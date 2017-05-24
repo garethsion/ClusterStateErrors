eda2c0aa627ca8f722316b4f5a9e962506e767ce
jamesrseddon <contact@james-seddon3d.co.uk>
2017-05-24 17:09:05 +0100

    Now raises error if noise parameter not between 0 and 1

--- James/noisyCluster.m
+++ James/noisyCluster.m
@@ -3,7 +3,13 @@ function [output] = noisyCluster(numberqubits,noiseparam)
 %%% |C><C|, the output will be rho = p(I/(2^n)) + (1-p)|C><C|, where p is
 %%% the variable noiseparam.
 
-
+if ~and(noiseparam>=0,noiseparam<=1)
+    msg = ['The noise parameter must be between'...
+            ' 0 (fully mixed) and 1 (pure).'];
+        error(msg);
+        return
+end
+    
 pureCluster = ClusterMaker(numberqubits);
 
 dim = 2^numberqubits;
