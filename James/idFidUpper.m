1541fef4712fbc22674f36c35315bc05188f8317
jamesrseddon <contact@james-seddon3d.co.uk>
2017-05-30 12:53:33 +0100

    Added function to calculate the correct fidelity bound for arbitrary M.

---
+++ James/idFidUpper.m
@@ -0,0 +1,23 @@
+function [output] = idFidUpper(alpha,M)
+%%% Calculates the upper bound for the fidelity using ID table formalism.
+%%% Input is experimental observable alpha and number of table rows M.
+
+
+if (abs(alpha)>M)
+    msg = 'Absolute value of alpha cannot be larger than M. Check inputs.';
+    error(msg)
+end
+
+if ~and((rem(M,1)==0),M>0)
+    msg = 'M needs to be a positive integer';
+    error(msg)
+end
+
+if mod(M,2)==0
+    % M is even
+    F_sq_ID_upper = (alpha + M)/(2*M);
+else
+    % M is odd
+    F_sq_ID_upper = (alpha + M - 2)/(2*M - 2);
+
+output = F_sq_ID_upper;
