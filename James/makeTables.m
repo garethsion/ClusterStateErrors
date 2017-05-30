6b8fbf1552ed03a4cfe2d9de672db3576d1c154f
jamesrseddon <contact@james-seddon3d.co.uk>
2017-05-30 13:53:15 +0100

    Added 6 qubit generator and ID tables.

--- James/makeTables.m
+++ James/makeTables.m
@@ -28,4 +28,19 @@ genMinusTable5 = [{'- X Z I I I'};
              {'I I Z X Z'};
              {'I I I Z X'}];
          
+genTable6 = [{'X Z I I I I'};
+             {'Z X Z I I I'};
+             {'I Z X Z I I'};
+             {'I I Z X Z I'};
+             {'I I I Z X Z'};
+             {'I I I I Z X'}];
+   
+         
+IDTable6 = [{'X Z I I I I'};
+             {'Z X Z I I I'};
+             {'I Z X Z I I'};
+             {'I I Z X Z I'};
+             {'I I I Z X Z'};
+             {'I I I I Z X'};
+             {'Y X X X X Y'}];
          
