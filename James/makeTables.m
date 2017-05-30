c846cef82808d25eb6c4ccf683000bc94857a58c
jamesrseddon <contact@james-seddon3d.co.uk>
2017-05-30 19:55:03 +0100

    Added a number of ID tables for the 4 qubit cluster state.

--- James/makeTables.m
+++ James/makeTables.m
@@ -1,5 +1,58 @@
 %%% Script to make ID tables and generator sets
+%%% 4-qubits
+genTable4 = [{'X Z I I'};
+             {'Z X Z I'};
+             {'I Z X Z'};
+             {'I I Z X'}];
 
+IDTable4 = [{'X Z I I'};
+             {'Z X Z I'};
+             {'I Z X Z'};
+             {'I I Z X'};
+             {'Y X X Y'}];
+         
+IDTable4a = [{'X Z I I'};
+             {'Y Y Z I'};
+             {'I Z X Z'};
+             {'I I Z X'};
+             {'- Z Y X Y'}];
+         
+IDTable4b = [{'X Z I I'};
+             {'Z X Z I'};
+             {'X I X Z'};
+             {'I I Z X'};
+             {'- Z Y X Y'}];
+         
+IDTable4c = [{'X Z I I'};
+             {'Z X Z I'};
+             {'I Z X Z'};
+             {'X Z Z X'};
+             {'- Z Y X Y'}];
+         
+IDTable4d = [{'X Z I I'};
+             {'Y Y Z I'};
+             {'X I X Z'};
+             {'X Z Z X'};
+             {'- Z Y X Y'}]; 
+         
+IDTable4e = [{'X Z I I'};
+             {'Y Y Z I'};
+             {'X I X Z'};
+             {'I I Z X'};
+             {'Y X X Y'}];
+
+IDTable4f = [{'X Z I I'};
+             {'Y Y Z I'};
+             {'I Z X Z'};
+             {'X Z Z X'};
+             {'Y X X Y'}];
+         
+IDTable4f = [{'X Z I I'};
+             {'Z X Z I'};
+             {'X I X Z'};
+             {'X Z Z X'};
+             {'Y X X Y'}];
+         
 %%% 5-qubit cluster state
 genTable5 = [{'X Z I I I'};
              {'Z X Z I I'};
@@ -27,6 +80,8 @@ genMinusTable5 = [{'- X Z I I I'};
              {'I Z X Z I'};
              {'I I Z X Z'};
              {'I I I Z X'}];
+
+%%% 6-qubits         
          
 genTable6 = [{'X Z I I I I'};
              {'Z X Z I I I'};
@@ -43,4 +98,27 @@ IDTable6 = [{'X Z I I I I'};
              {'I I I Z X Z'};
              {'I I I I Z X'};
              {'Y X X X X Y'}];
+      
+
+genTable6a = [{'- X Z I I I I'};
+             {'Z X Z I I I'};
+             {'I Z X Z I I'};
+             {'I I Z X Z I'};
+             {'I I I Z X Z'};
+             {'I I I I Z X'}];
+         
+genTable6b = [{'- X Z I I I I'};
+             {'- Z X Z I I I'};
+             {'I Z X Z I I'};
+             {'I I Z X Z I'};
+             {'I I I Z X Z'};
+             {'I I I I Z X'}];
+         
+         
+genTable6c = [{'- X Z I I I I'};
+             {'- Z X Z I I I'};
+             {'- I Z X Z I I'};
+             {'I I Z X Z I'};
+             {'I I I Z X Z'};
+             {'I I I I Z X'}];
          
