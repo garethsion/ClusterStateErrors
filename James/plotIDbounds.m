93d076d54530a92f5a5b8f0edf27abc68b8b9553
jamesrseddon <contact@james-seddon3d.co.uk>
2017-05-30 13:54:06 +0100

    Added flag so that bounds can either be plotted on a new figure, or added to an existing one.

--- James/plotIDbounds.m
+++ James/plotIDbounds.m
@@ -1,6 +1,13 @@
-function [output] = plotIDbounds(M)
+function [output] = plotIDbounds(M,varargin)
 %%% Plot the upper and lower fidelity squared bounds given by the ID formalism, for
 %%% the ID table with M rows, as a function of alpha.
+%%% Second argument is a flag
+
+nofigureflag = 0;
+if nargin>1
+    nofigureflag = varargin{1};
+    display(['Yay!' num2str(nofigureflag)]);
+end
 
 for k = 1:100
     alpha(k) = M*(k-50)/50;
@@ -8,12 +15,19 @@ for k = 1:100
     F_ID_up(k) = idFidUpper(alpha(k),M);
 end
 
-figure;
-hold on
+
+if nofigureflag == 0
+    figure;
+else
+    hold on
+end
 plot(alpha,F_ID_low,'--b');
 hold on
 plot(alpha,F_ID_up,'--r');
-xlabel('<alpha>');
-ylabel('Fidelity squared');
-legend('Lower bound','Upper bound','Location','southeast');
-ylim([0 1]);
+
+if nofigureflag == 0
+    xlabel('<alpha>');
+    ylabel('Fidelity squared');
+    legend('Lower bound','Upper bound','Location','southeast');
+    ylim([0 1]);
+end
