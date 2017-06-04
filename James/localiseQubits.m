4e5876ddd03507620ef85020d4e08b39b9c45318
jamesrseddon <contact@james-seddon3d.co.uk>
2017-06-04 01:38:21 +0100

    Added function to project cluster state then partial trace out target qubits.

---
+++ James/localiseQubits.m
@@ -0,0 +1,30 @@
+function [output] = localiseQubits(measurementOutcomes,rho,qubit_1,qubit_2);
+%%%% Project out a pair of qubits based on measurement outcomes, and
+%%%% partial trace. qubit_1 and qubit_2 are the qubits to retain, e.g. for
+%%%% a five qubit state, qubit_1=2 and qubit_2=5 traces out qubits 1, 3,
+%%%% and 4.
+c
+number_qubits = log2(size(rho,1))
+
+number_outcomes = length(measurementOutcomes);
+
+projection_results = pauliProject(measurementOutcomes,rho);
+
+dimension_vector = 2*ones(1,number_qubits);
+
+trace_vector = [1:number_qubits];
+
+trace_vector = trace_vector(trace_vector~=qubit_1);
+
+trace_vector = trace_vector(trace_vector~=qubit_2);
+
+for k=1:number_outcomes
+    this_state = projection_results{k,3};
+    reduced_state = PartialTrace(this_state,trace_vector,dimension_vector);
+    results(k,:) = {projection_results{k,:},reduced_state};
+end
+    
+output = results;
+    
+    
+    
