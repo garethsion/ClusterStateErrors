9ddc47a01ede567046ed8a801799d0cd84e20093
jamesrseddon <contact@james-seddon3d.co.uk>
2017-06-06 18:50:01 +0100

    Added script to plot graphs from calculated results

---
+++ analysis/plotGraphScript.m
@@ -0,0 +1,31 @@
+%%%%%%%%%%%%%% plot concurrence results %%%%%%%%%%%%%
+
+result_table = eval_results;
+parameter_column = 1;
+parameter_name = nicetable{1,1};
+y_axis_label = 'Concurrence bound';
+
+y_value_columns = [8,9,10,11,12];
+% LE_column = 8;
+% C_ZXZ_column = 9;
+% C_gen_column = 10;
+% C_alpha_column = 11;
+% C_triplet_column = 12;
+
+num_quantities = length(y_value_columns);
+
+param_values = result_table(:,parameter_column);
+
+figure;
+
+
+for j = 1:num_quantities
+    y_values = result_table(:,y_value_columns(j));
+    legend_parts{1,j} = nicetable{1,y_value_columns(j)};
+    plot(param_values,y_values);
+    hold on;
+end
+
+xlabel(parameter_name);
+ylabel(y_axis_label);
+legend(legend_parts);
